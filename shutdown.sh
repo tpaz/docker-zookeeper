13b6255dca8f85f4418e6981cad690533a450eb4
Tomer Paz <tomer.paz@gmail.com>
2015-01-12 16:49:04 +0200

    automatic shutdown of all zookeeper containers (no matter how many as long as their image name has 'zookeeper' in it) and removal comment out kafka+mini-app-api

--- shutdown.sh
+++ shutdown.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-for i in {1..4} ; do
-  docker rm -f kafka${i}
+CONTAINERS=`docker ps | grep "NAMES\|zookeeper" | awk 'NR==1{ NAMESPOS=index($0,"NAMES"); } NR>1{  NAMES=substr($0, NAMESPOS); print NAMES }'`
+
+#for i in {1..4} ; do
+#  docker rm -f kafka${i}
+#done
+
+for i in ${CONTAINERS} ; do
+  docker stop ${i}
+  docker rm -f ${i}
 done
 
-for i in {1..5} ; do
-  docker rm -f zoo${i}
-done
-
-for i in {1..5} ; do
-  docker rm -f api${i}
-done
+#for i in {1..5} ; do
+#  docker rm -f api${i}
+#done
