98155608bc8c1f73edc8a78c54198ebad93ccb40
Tomer Paz <tomer.paz@gmail.com>
2015-01-12 12:09:54 +0200

    support configurable number of zookeeper intstances remove my-little-api

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 docker-zookeeper
 ================
 
-A 5 node zookeeper ensemble that runs in Docker
+A configurable # of nodes zookeeper ensemble that runs in Docker
 
 Note: the wouterd/zookeeper image is in the docker index as an "automated build", so you don't have to build the 
 Dockerfile in the zookeeper folder.
 
 Start ensemble by running:
 
-    ./startup.sh
+    ./startup.sh <# of nodes>
     
 From the root of the project.
 
 This will:
 
 - Create a config container
-- Create 5 zookeeper containers that wait for a config file to appear on the config container
+- Create n-1 zookeeper containers that wait for a config file to appear on the config container
 - Generate the configuration
 - Push the configuration into the config container
 - Start the ensemble
--- startup.sh
+++ startup.sh
@@ -2,19 +2,32 @@
 
 set -e
 
+ZOOKEEPER_NODES=5
+
+if [ $# -lt 1 ] ; then
+  echo "Usage: $0 <# of zookeeper nodes>. Minimum 2"
+  echo "Using default: 5" 
+else
+  if [ $1 -lt 2 ] ; then
+    echo "not enough nodes. minimum set to 2"
+    exit 1
+  fi
+  ZOOKEEPER_NODES=$1
+fi
+
 if [ ! ${nopull} ] ; then
   echo 'Pulling all images needed, just to be sure'
   docker pull busybox
   docker pull wouterd/zookeeper
-  docker pull wouterd/kafka
+#  docker pull wouterd/kafka
 fi
 
 # Need a volume to read the config from
 conf_container=zoo1
 
 # Start the zookeeper containers
-for i in {1..5} ; do
-  if [ "${i}" == "1" ] ; then
+for i in $(seq 1 $ZOOKEEPER_NODES) ; do
+  if [ ${i} -eq 1 ] ; then
     docker run -d -v /zoo/conf --name "zoo${i}" -e ZOO_ID=${i} wouterd/zookeeper
   else
     docker run -d --volumes-from ${conf_container} --name "zoo${i}" -e ZOO_ID=${i} wouterd/zookeeper
@@ -24,7 +37,7 @@ done
 config=$(cat zoo.cfg.initial)
 
 # Look up the zookeeper instance IPs and create the config file
-for i in {1..5} ; do
+for i in $(seq 1 $ZOOKEEPER_NODES) ; do
   container_name=zoo${i}
   container_ip=$(docker inspect --format '{{.NetworkSettings.IPAddress}}' ${container_name})
   line="server.${i}=${container_ip}:2888:3888"
@@ -34,15 +47,17 @@ done
 # Write the config to the config container
 echo "${config}" | docker run -i --rm --volumes-from ${conf_container} busybox sh -c 'cat > /zoo/conf/zoo.cfg'
 
-zoo_links='--link zoo1:zoo1 --link zoo2:zoo2 --link zoo3:zoo3 --link zoo4:zoo4 --link zoo5:zoo5'
-zoo_connect='zoo1:2181,zoo2:2181,zoo3:2181,zoo4:2181,zoo5:2181'
-
-# Start 4 Kafka's
-# for i in {1..4} ; do
-#   docker run -d ${zoo_links} -e BROKER_ID=${i} -e ZOO=${zoo_connect} --name kafka${i} -h kafka${i} wouterd/kafka
-# done
-
-# Start 5 apis
-for i in {1..5} ; do
-  docker run -d ${zoo_links} -e ZOO=${zoo_connect} --name api${i} my-little-api 
+zoo_links=''
+zoo_connect=''
+for i in $(seq 1 $ZOOKEEPER_NODES) ; do
+  zoo_links="${zoo_links}--link zoo${i}:zoo${i} "
+  zoo_connect="${zoo_connect}zoo${i}"
+  if [ $i -lt ${ZOOKEEPER_NODES} ] ; then
+    zoo_connect="${zoo_connect},"
+  fi
 done
+
+# Start ZOOKEEPER_NODES apis
+#for i in $(seq 1 $ZOOKEEPER_NODES) ; do
+#  docker run -d ${zoo_links} -e ZOO=${zoo_connect} --name api${i} my-little-api 
+#done
